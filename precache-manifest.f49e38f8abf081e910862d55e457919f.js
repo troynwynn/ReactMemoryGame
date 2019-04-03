b36e5842e753284b4cbf692ab297dee642f671e5
Troy Wynn <troynwynn@gmail.com>
2019-04-03 18:52:35 -0400

    Updates

---
+++ precache-manifest.f49e38f8abf081e910862d55e457919f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "834b4ac589a3a1aeb945",
+    "url": "/reactmemorygame/static/js/runtime~main.e9c274db.js"
+  },
+  {
+    "revision": "8dd45deb5ad033467523",
+    "url": "/reactmemorygame/static/js/main.51df5feb.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/reactmemorygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8dd45deb5ad033467523",
+    "url": "/reactmemorygame/static/css/main.15553d2d.chunk.css"
+  },
+  {
+    "revision": "8188aa68296782d17517a801e1c51d96",
+    "url": "/reactmemorygame/index.html"
+  }
+];
